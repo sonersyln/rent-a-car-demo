d997afe0e748c44ace7403c2ce65809932feb6ca
ShmsErl <erol.sehmus1@gmail.com>
2023-11-17 19:18:23 +0300

    updat

--- src/main/java/com/example/rent_a_car_demo/models/Brand.java
+++ src/main/java/com/example/rent_a_car_demo/models/Brand.java
@@ -1,4 +1,24 @@
 package com.example.rent_a_car_demo.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "brands")
 public class Brand {
+    @Id
+    @Column(name = "id",  nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id ;
+    @Column(name = "name", nullable = false)
+    private String name ;
+    @OneToMany(mappedBy = "brand")
+    private List<Model> models;
+
 }
--- src/main/java/com/example/rent_a_car_demo/models/Model.java
+++ src/main/java/com/example/rent_a_car_demo/models/Model.java
@@ -1,4 +1,36 @@
 package com.example.rent_a_car_demo.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "models")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "fuel_type", length = 20, nullable = false)
+    private String fuelType;
+
+    @Column(name = "engine_power", length = 5, nullable = false)
+    private String enginePower;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id", nullable = false)
+    private Brand brand;
 }
+
+
+
