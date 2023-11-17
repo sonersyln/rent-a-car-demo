73038cf1f874b3af29e5d6246db3964ec9ac82da
ShmsErl <erol.sehmus1@gmail.com>
2023-11-17 19:19:05 +0300

    update

--- src/main/java/com/example/rent_a_car_demo/models/Model.java
+++ src/main/java/com/example/rent_a_car_demo/models/Model.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.validation.annotation.Validated;
 
 @Entity
 @Table(name = "models")
