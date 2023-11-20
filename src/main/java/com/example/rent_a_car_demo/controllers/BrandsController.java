6fb5e71aa5acd7c68cb2e632ffd662a79604bef1
ShmsErl <erol.sehmus1@gmail.com>
2023-11-20 17:47:17 +0300

    Brand ve Model Nesneleri için veri katmanı, service katmanı ve Controller katmanı eklendi CRUD işlemleri tanımlandı

--- src/main/java/com/example/rent_a_car_demo/controllers/BrandsController.java
+++ src/main/java/com/example/rent_a_car_demo/controllers/BrandsController.java
@@ -1,4 +1,40 @@
 package com.example.rent_a_car_demo.controllers;
 
+import com.example.rent_a_car_demo.models.Brand;
+import com.example.rent_a_car_demo.services.BrandService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping()
+@AllArgsConstructor
 public class BrandsController {
+
+    private final BrandService brandService;
+    @GetMapping("/getall")
+    public List<Brand> getBrandList() {
+
+       return  this.brandService.getBrandList();
+    }
+
+    @PostMapping()
+    public String createBrand(@RequestBody Brand brand) {
+
+        return this.brandService.createBrand(brand);
+    }
+
+    @PutMapping("/{id}")
+    public String updateBrand(@PathVariable int id, @RequestBody Brand brand) throws Exception {
+
+        return this.brandService.updateBrand(id, brand);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBrand(@PathVariable int id) throws Exception {
+
+        return this.brandService.deleteByBrand(id);
+    }
 }
--- src/main/java/com/example/rent_a_car_demo/controllers/ModelController.java
+++ src/main/java/com/example/rent_a_car_demo/controllers/ModelController.java
@@ -1,4 +1,45 @@
 package com.example.rent_a_car_demo.controllers;
 
+import com.example.rent_a_car_demo.models.Brand;
+import com.example.rent_a_car_demo.models.Model;
+import com.example.rent_a_car_demo.services.BrandService;
+import com.example.rent_a_car_demo.services.ModelService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping()
+@AllArgsConstructor
 public class ModelController {
+
+    private ModelService modelService;
+
+
+
+    @GetMapping("/getall")
+    public List<Model> getBrandList() {
+
+        return  this.modelService.getModelList();
+    }
+
+    @PostMapping()
+    public String createBrand(@RequestBody Model model) {
+
+        return this.modelService.createModel(model);
+    }
+
+    @PutMapping("/{id}")
+    public String updateBrand(@PathVariable int id, @RequestBody Model brand) throws Exception {
+
+        return this.modelService.updateModel(id, brand);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBrand(@PathVariable int id) throws Exception {
+
+        return this.modelService.deleteByModel(id);
+    }
 }
