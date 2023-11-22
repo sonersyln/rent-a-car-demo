2b311962c287a038b91cd3937986bf92cf47c37e
ShmsErl <erol.sehmus1@gmail.com>
2023-11-22 20:10:48 +0300

    User ve Adress Nesneleri için DTO sınıfları oluşturuldu.

---
+++ src/main/java/com/example/rent_a_car_demo/dtos/requests/AddUserRequest.java
@@ -0,0 +1,30 @@
+package com.example.rent_a_car_demo.dtos.requests;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddUserRequest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private String gender;
+
+    private Date birthDate;
+}
