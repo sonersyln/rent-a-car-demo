8a52b36672be290b443c366703f9eda84ef2431d
Soner <syln.soner@gmail.com>
2023-11-22 17:46:29 +0300

    Brand, Car, CarType modelleri için dtos,controllers, services paketleri yazıldı.

--- src/main/java/com/example/rent_a_car_demo/controllers/CarTypeController.java
+++ src/main/java/com/example/rent_a_car_demo/controllers/CarTypeController.java
@@ -1,44 +1,44 @@
 package com.example.rent_a_car_demo.controllers;
 
-import com.example.rent_a_car_demo.models.CarType;
+import com.example.rent_a_car_demo.dtos.requests.AddCarTypeRequest;
+import com.example.rent_a_car_demo.dtos.requests.UpdateCarTypeRequest;
+import com.example.rent_a_car_demo.dtos.responses.GetCarTypeListResponse;
+import com.example.rent_a_car_demo.dtos.responses.GetCarTypeResponse;
 import com.example.rent_a_car_demo.services.CarTypeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/cartypes")
 public class CarTypeController {
     private final CarTypeService carTypeService;
 
-    @Autowired
-    public CarTypeController(CarTypeService carTypeService) {
-        this.carTypeService = carTypeService;
-    }
 
     @GetMapping("/getall")
-    public List<CarType> getAllTypes() {
+    public List<GetCarTypeListResponse> getAllTypes() {
         return carTypeService.getAllTypes();
     }
 
     @GetMapping("/get")
-    public CarType getCarTypeById(@RequestParam(value = "id") Integer id) {
+    public GetCarTypeResponse getCarTypeById(@RequestParam(value = "id") Integer id) {
         return carTypeService.getCarTypeById(id);
     }
 
     @PostMapping("/add")
-    public CarType saveCarType(@RequestBody CarType carType) {
-        return carTypeService.saveCarType(carType);
+    public String saveCarType(@RequestBody AddCarTypeRequest addCarTypeRequest) {
+        return carTypeService.saveCarType(addCarTypeRequest);
     }
 
     @PutMapping()
-    public void updateCarType(@RequestBody CarType carType) {
-        carTypeService.updateCarType(carType);
+    public String updateCarType(@RequestBody UpdateCarTypeRequest updateCarTypeRequest) throws Exception {
+       return carTypeService.updateCarType(updateCarTypeRequest);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCarType(@PathVariable Integer id) {
-        carTypeService.deleteCarType(id);
+    public String deleteCarType(@PathVariable Integer id) throws Exception {
+       return carTypeService.deleteCarType(id);
     }
 }
---
+++ src/main/java/com/example/rent_a_car_demo/dtos/requests/AddCarTypeRequest.java
@@ -0,0 +1,11 @@
+package com.example.rent_a_car_demo.dtos.requests;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddCarTypeRequest {
+    private String name;
+}
