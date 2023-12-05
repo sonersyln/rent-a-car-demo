0712ab5ea4e6acaba9dae2b67586a5a3be47b47a
Soner <syln.soner@gmail.com>
2023-12-05 22:03:36 +0300

    EmployeeRepository güncellendi.

--- src/main/java/com/example/rent_a_car_demo/repositories/EmployeeRepository.java
+++ src/main/java/com/example/rent_a_car_demo/repositories/EmployeeRepository.java
@@ -4,5 +4,5 @@ import com.example.rent_a_car_demo.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-    findByUser
+
 }
